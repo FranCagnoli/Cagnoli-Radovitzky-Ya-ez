49d359a12b82d5ac490bbf30c75092c1663b0858
Hernan Yáñez <heryanez212@gmail.com>
2022-05-12 18:19:08 -0300

    added tests for ElectricCharginPoint domain - red stage

---
+++ Ministerio de Turismo/MinTur.Domain.Test/BusinessEntities/ElectricChargingPointTest.cs
@@ -0,0 +1,144 @@
+﻿using MinTur.Domain.BusinessEntities;
+using MinTur.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MinTur.Domain.Test.BusinessEntities
+{
+    [TestClass]
+    public class ElectricChargingPointTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithNoNameValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Address = "Una direccion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithLongNameValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga con nombre largo",
+                Address = "Una direccion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithNoAddressFailsValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithLongAddressFailsValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion muy larga para la validacion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithNoRegionFailsValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion",
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithNoDescriptionFailsValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidRequestDataException))]
+        public void ElectricChargingPointWithLongDescriptionFailsValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion muy larga para el campo, Una Descripcion muy larga para el campo.",
+
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+
+        [TestMethod]
+        public void ValidElectricChargingPointPassesValidation()
+        {
+            ElectricChargingPoint ElectricChargingPoint = new ElectricChargingPoint()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion",
+                Region = new Region()
+                {
+                    Name = "Region"
+                },
+                Description = "Una Descripcion",
+            };
+            ElectricChargingPoint.ValidOrFail();
+        }
+
+   
+
+    }
+}
