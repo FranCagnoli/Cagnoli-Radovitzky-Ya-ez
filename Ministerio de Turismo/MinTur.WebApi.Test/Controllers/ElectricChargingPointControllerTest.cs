30bed569fb8c08a890729e7ddc043c6ce6d62204
Hernan Yáñez <heryanez212@gmail.com>
2022-05-13 12:12:39 -0300

    addded ElectricChargingPoint WebAPI controller

---
+++ Ministerio de Turismo/MinTur.WebApi.Test/Controllers/ElectricChargingPointControllerTest.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using MinTur.BusinessLogicInterface.ResourceManagers;
+using MinTur.Domain.BusinessEntities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MinTur.Models.In;
+using MinTur.Models.Out;
+using Moq;
+using MinTur.WebApi.Controllers;
+
+namespace MinTur.WebApi.Test.Controllers
+{
+    [TestClass]
+    public class ElectricChargingPointControllerTest
+    {
+        private List<ElectricChargingPoint> _ElectricChargingPoints;
+        private List<ElectricChargingPointBasicInfoModel> _ElectricChargingPointBasicInfoModels;
+        private Mock<IElectricChargingPointManager> _ElectricChargingPointManagerMock;
+
+        #region SetUp
+        [TestInitialize]
+        public void SetUp()
+        {
+            _ElectricChargingPoints = new List<ElectricChargingPoint>();
+            _ElectricChargingPointBasicInfoModels = new List<ElectricChargingPointBasicInfoModel>();
+            _ElectricChargingPointManagerMock = new Mock<IElectricChargingPointManager>(MockBehavior.Strict);
+
+            LoadElectricChargingPoints();
+            LoadElectricChargingPointBasicInfoModels();
+        }
+
+        private void LoadElectricChargingPoints()
+        {
+            ElectricChargingPoint ElectricChargingPoint1 = new ElectricChargingPoint()
+            {
+                Id = 1,
+                Name = "Puerto1",
+                RegionId = 2,
+                Region = new Region()
+                {
+                    Id = 2,
+                    Name = "Region"
+                },
+                Description = "Desc",
+                Address = "Direcc"
+            };
+
+            ElectricChargingPoint ElectricChargingPoint2 = new ElectricChargingPoint()
+            {
+                Id = 2,
+                Name = "Puerto2",
+                RegionId = 2,
+                Region = new Region()
+                {
+                    Id = 2,
+                    Name = "Region"
+                },
+                Description = "Desc",
+                Address = "Direcc"
+            };
+            _ElectricChargingPoints.Add(ElectricChargingPoint1);
+            _ElectricChargingPoints.Add(ElectricChargingPoint2);
+        }
+
+
+        private void LoadElectricChargingPointBasicInfoModels()
+        {
+            foreach (ElectricChargingPoint ElectricChargingPoint in _ElectricChargingPoints)
+            {
+                _ElectricChargingPointBasicInfoModels.Add(new ElectricChargingPointBasicInfoModel(ElectricChargingPoint));
+            }
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void GetAllElectricChargingPointsOkTest()
+        {
+            _ElectricChargingPointManagerMock.Setup(c => c.GetAllElectricChargingPoints()).Returns(_ElectricChargingPoints);
+            ElectricChargingPointController ElectricChargingPointController = new ElectricChargingPointController(_ElectricChargingPointManagerMock.Object);
+
+            IActionResult result = ElectricChargingPointController.GetAll();
+            OkObjectResult okResult = result as OkObjectResult;
+            List<ElectricChargingPointBasicInfoModel> responseModel = okResult.Value as List<ElectricChargingPointBasicInfoModel>;
+
+            _ElectricChargingPointManagerMock.VerifyAll();
+            CollectionAssert.AreEquivalent(responseModel, _ElectricChargingPointBasicInfoModels);
+        }
+
+        [TestMethod]
+        public void CreateElectricChargingPointCreatedTest()
+        {
+            ElectricChargingPointIntentModel ElectricChargingPointIntentModel = CreateElectricChargingPointIntentModel();
+            ElectricChargingPoint createdElectricChargingPoint = CreateElectricChargingPoint();
+
+            _ElectricChargingPointManagerMock.Setup(a => a.RegisterElectricChargingPoint(It.IsAny<ElectricChargingPoint>())).Returns(createdElectricChargingPoint);
+            ElectricChargingPointController ElectricChargingPointController = new ElectricChargingPointController(_ElectricChargingPointManagerMock.Object);
+
+            IActionResult result = ElectricChargingPointController.CreateElectricChargingPoint(ElectricChargingPointIntentModel);
+            CreatedResult createdResult = result as CreatedResult;
+
+            _ElectricChargingPointManagerMock.VerifyAll();
+            Assert.IsTrue(createdResult.Value.Equals(new ElectricChargingPointBasicInfoModel(createdElectricChargingPoint)));
+        }
+
+        [TestMethod]
+        public void GetElectricChargingPointByIdOkTest()
+        {
+            int ElectricChargingPointId = 4;
+            ElectricChargingPoint expectedElectricChargingPoint = CreateElectricChargingPointWithSpecificId(ElectricChargingPointId);
+
+            _ElectricChargingPointManagerMock.Setup(a => a.GetElectricChargingPointById(ElectricChargingPointId)).Returns(expectedElectricChargingPoint);
+            ElectricChargingPointController ElectricChargingPointController = new ElectricChargingPointController(_ElectricChargingPointManagerMock.Object);
+
+            IActionResult result = ElectricChargingPointController.GetSpecificElectricChargingPoint(ElectricChargingPointId);
+            OkObjectResult okResult = result as OkObjectResult;
+
+            _ElectricChargingPointManagerMock.VerifyAll();
+            Assert.AreEqual(new ElectricChargingPointBasicInfoModel(expectedElectricChargingPoint), okResult.Value);
+        }
+
+        #region Helpers
+        public ElectricChargingPointIntentModel CreateElectricChargingPointIntentModel()
+        {
+            return new ElectricChargingPointIntentModel()
+            {
+                Name = "Puerto de carga 1",
+                Address = "Una direccion",
+                RegionId = 1,
+                Description = "Una Descripcion",
+            };
+        }
+
+        public ElectricChargingPoint CreateElectricChargingPoint()
+        {
+            return new ElectricChargingPoint()
+            {
+                Id = 1,
+                Name = "Puerto1",
+                RegionId = 2,
+                Region = new Region()
+                {
+                    Id = 2,
+                    Name = "Region"
+                },
+                Description = "Desc",
+                Address = "Direcc"
+            };
+
+        }
+        public ElectricChargingPoint CreateElectricChargingPointWithSpecificId(int ElectricChargingPointId)
+        {
+            return new ElectricChargingPoint()
+            {
+                Id = ElectricChargingPointId,
+                Name = "Puerto1",
+                RegionId = 2,
+                Region = new Region()
+                {
+                    Id = 2,
+                    Name = "Region"
+                },
+                Description = "Desc",
+                Address = "Direcc"
+            };
+
+        }
+        #endregion
+    }
+}
---
+++ Ministerio de Turismo/MinTur.WebApi/Controllers/ElectricChargingPointController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinTur.BusinessLogicInterface.ResourceManagers;
+using MinTur.Domain.BusinessEntities;
+using MinTur.Models.In;
+using MinTur.Models.Out;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MinTur.WebApi.Controllers
+{
+    [EnableCors("AllowEverything")]
+    [Route("api/electricChargingPoints")]
+    [ApiController]
+    public class ElectricChargingPointController : Controller
+    {
+
+        private readonly IElectricChargingPointManager _ElectricChargingPointManager;
+
+        public ElectricChargingPointController(IElectricChargingPointManager ElectricChargingPointManager)
+        {
+            _ElectricChargingPointManager = ElectricChargingPointManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<ElectricChargingPoint> retrievedElectricChargingPoints = _ElectricChargingPointManager.GetAllElectricChargingPoints();
+            List<ElectricChargingPointBasicInfoModel> ElectricChargingPointBasicInfoModels = retrievedElectricChargingPoints.Select(ElectricChargingPoint => new ElectricChargingPointBasicInfoModel(ElectricChargingPoint)).ToList();
+            return Ok(ElectricChargingPointBasicInfoModels);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetSpecificElectricChargingPoint(int id)
+        {
+            ElectricChargingPoint retrievedElectricChargingPoint = _ElectricChargingPointManager.GetElectricChargingPointById(id);
+            ElectricChargingPointBasicInfoModel ElectricChargingPointBasicInfoModel = new ElectricChargingPointBasicInfoModel(retrievedElectricChargingPoint);
+            return Ok(ElectricChargingPointBasicInfoModel);
+        }
+
+        [HttpPost]
+        public IActionResult CreateElectricChargingPoint([FromBody] ElectricChargingPointIntentModel ElectricChargingPointIntentModel)
+        {
+            ElectricChargingPoint createdElectricChargingPoint = _ElectricChargingPointManager.RegisterElectricChargingPoint(ElectricChargingPointIntentModel.ToEntity());
+            ElectricChargingPointBasicInfoModel confirmation = new ElectricChargingPointBasicInfoModel(createdElectricChargingPoint);
+            return Created("api/electricChargingPoints/" + createdElectricChargingPoint.Id, confirmation);
+        }
+
+    }
+}
