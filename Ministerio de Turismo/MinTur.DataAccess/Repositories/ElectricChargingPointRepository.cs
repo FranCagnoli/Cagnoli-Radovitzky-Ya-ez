e314919d7866d0e482d3100432fad8bab2bef8bb
Hernan Yáñez <heryanez212@gmail.com>
2022-05-13 13:59:19 -0300

    fixed issue with regionId

--- Ministerio de Turismo/MinTur.DataAccess/Repositories/ElectricChargingPointRepository.cs
+++ Ministerio de Turismo/MinTur.DataAccess/Repositories/ElectricChargingPointRepository.cs
@@ -19,7 +19,7 @@ namespace MinTur.DataAccess.Repositories
 
         public List<ElectricChargingPoint> GetAllElectricChargingPoints()
         {
-            return Context.Set<ElectricChargingPoint>().ToList();
+            return Context.Set<ElectricChargingPoint>().Include(ecp => ecp.Region).ToList();
         }
 
         public ElectricChargingPoint GetElectricChargingPointById(int id)
@@ -35,18 +35,18 @@ namespace MinTur.DataAccess.Repositories
 
         public int StoreElectricChargingPoint(ElectricChargingPoint newElectricChargingPoint)
         {
-            if (!RegionExists(newElectricChargingPoint.Region))
+            if (!RegionExists(newElectricChargingPoint.RegionId))
                 throw new ResourceNotFoundException("Could not find specified region");
 
-            newElectricChargingPoint.Region = Context.Set<Region>().Where(r => r.Id == newElectricChargingPoint.Region.Id).FirstOrDefault();
+            newElectricChargingPoint.Region = Context.Set<Region>().Where(r => r.Id == newElectricChargingPoint.RegionId).FirstOrDefault();
             StoreInDb(newElectricChargingPoint);
 
             return newElectricChargingPoint.Id;
         }
 
-        private bool RegionExists(Region region)
+        private bool RegionExists(int regionId)
         {
-            return Context.Set<Region>().Any(r => r.Id == region.Id);
+            return Context.Set<Region>().Any(r => r.Id == regionId);
         }
 
         private void StoreInDb(ElectricChargingPoint newElectricChargingPoint)
